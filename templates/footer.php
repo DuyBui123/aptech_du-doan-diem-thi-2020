7b554a91232047b58ee30ff1451f591a929401e1
censodev <51000027+censodev@users.noreply.github.com>
2020-08-01 23:52:40 +0700

    init

---
+++ templates/footer.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<footer class="text-white text-md-left text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 col-md-8 order-12 order-lg-1">
+                <h3 class="text-uppercase text-bold mb-3 footer-heading">Hệ thống đào tạo lập trình viên quốc tế Aptech</h3>
+                <table border="0" class="w-100">
+                    <tbody>
+                        <tr>
+                            <td class="text-bold pr-3">Hà Nội:</td>
+                            <td><span class="text-bold">285 Đội Cấn</span>, Q. Ba Đình</td>
+                            <td class="text-bold px-3">Tel: <a href="tel:0899 179 029" class="text-white">0899 179 029</a></td>
+                            <td>Email: <a href="mailto:aptech1@aprotrain.com" target="_blank" class="text-white">aptech1@aprotrain.com</a></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><span class="text-bold">54 Lê Thanh Nghị</span>, Q. Hai Bà Trưng</td>
+                            <td class="text-bold px-3">Tel: <a href="tel:0899 179 029" class="text-white">0899 179 029</a></td>
+                            <td>Email: <a href="mailto:aptech3@aprotrain.com" target="_blank" class="text-white">aptech3@aprotrain.com</a></td>
+                        </tr>
+                        <tr>
+                            <td class="text-bold pr-3 pt-3" rowspan="2">TP.HCM:</td>
+                            <td class="pt-0 pt-md-3"><span class="text-bold">212 - 214 Nguyễn Đình Chiểu</span>, P.6, Q.3</td>
+                            <td class="text-bold px-3 pt-0 pt-md-3">Tel: <a href="tel:1800 1779" class="text-white">1800 1779</a></td>
+                            <td class="pt-0 pt-md-3">Email: <a href="mailto:aptech2@aprotrain.com" target="_blank" class="text-white">aptech2@aprotrain.com</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-3 col-md-4 mb-4 mb-md-0 order-1 order-lg-12">
+                <a href="https://aptechvietnam.com.vn/" target="_blank" class="logo-footer text-center text-md-left">
+                    <img src="./assets/img/logo-footer.png" class="mw-100">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ templates/page-1.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" href="./assets/css/page-1.css">
+
+<div class="row pb-5">
+    <div class="col-lg-6 col-md-6 col-sm-12 pt-5">
+        <h1 class="text-main text-magic">Round 1: Dự đoán điểm thi</h1>
+        <ul class="text-bold mt-4">
+            <li class="d-flex align-items-start">
+                <img class="mr-2" src="./assets/img/dot.png" alt="" srcset="">
+                <span>Bạn nghĩ mình sẽ đạt bao nhiêu điểm trong kỳ thi THPT và Đại học sắp tới?</span>
+            </li>
+            <li class="d-flex align-items-start">
+                <img class="mr-2" src="./assets/img/dot.png" alt="" srcset="">
+                <span>Cùng tham gia dự đoán kết quả và nhận quà cùng Aptech nhé!</span>
+            </li>
+            <li class="d-flex align-items-start">
+                <img class="mr-2" src="./assets/img/dot.png" alt="" srcset="">
+                <span>Đừng quên quay trở lại tham gia phần 2 của game vào ngày 27/08 khi có kết quả thực tế nha!</span>
+            </li>
+            <li class="d-flex align-items-start">
+                <img class="mr-2" src="./assets/img/dot.png" alt="" srcset="">
+                <span>Các bạn nhớ like Fanpage <a class="text-main" href="https://www.facebook.com/aptechvietnam.com.vn">Hệ thống Đào tạo Lập trình viên Quốc tế Aptech</a> và làm đủ các bước điều kiện để nhận quà nhé!</span>
+            </li>
+        </ul>
+        <p class="text-italic">(Lưu ý nè: Số điểm được tính trên tổng điểm 3 môn thuộc khối thi của bạn không nhân hệ số nha! <img src="./assets/img/smile.png" alt=""> )</p>
+        <div class="d-flex justify-content-center pt-4">
+            <img class="mw-100" src="./assets/img/page-1-gift.png" alt="">
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12 pt-5">
+        <form action="" method="post" class="d-flex flex-column justify-content-center align-items-center py-4 mt-3">
+            <label class="text-italic text-center">Thông tin người chơi:</label>
+            <input class="input-magic mb-2" type="text" placeholder="Họ và tên">
+            <input class="input-magic mb-2" type="text" placeholder="Số điện thoại">
+            <input class="input-magic mb-2" type="email" placeholder="Email">
+            <input class="input-magic mb-2" type="text" placeholder="Tỉnh/Thành phố">
+            <input class="input-magic mb-2" type="text" placeholder="Khối thi">
+            <div class="area-magic mb-3">
+                <h5 class="text-main text-bold text-center">ĐIỂM DỰ ĐOÁN</h5>
+                <input type="number">
+            </div>
+            <input type="submit" value="GỬI DỰ ĐOÁN" class="text-white px-4 text-bold">
+        </form>
+    </div>
+</div>
