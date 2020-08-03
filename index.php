02a3cfa939532bdcab042a303674737317456c6a
censodev <buiphuongaptutc@gmail.com>
2020-08-03 11:52:35 +0700

    fix thumbnail fb

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aptech | Dự đoán điểm thi</title>
 
-    <meta property="og:image" content="<?php echo DOMAIN ?>/assets/img/banner.png" />
+    <meta property="og:image" content="<?php echo DOMAIN ?>/assets/img/shared_bg.png" />
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/master.css">
